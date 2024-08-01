70428434ecbd79d58cc0a658e4b855283dc78bc7
Vitalforce88 <egorovandrey.88@yandex.ru>
2024-08-01 15:36:17 +0300

    Modified task 1 for lesson 7, Added the case when m > n

--- Program.cs
+++ Program.cs
@@ -9,40 +9,50 @@
 
 void NumbersOnSegment(int m, int n, bool flag = true)
 {
-    
-    if(m == n)
+
+    if (m == n)
     {
         System.Console.Write($"{m} ]");
         return;
     }
-    if(flag == true) System.Console.Write("[ ");
-    System.Console.Write($"{m}, ");
-    NumbersOnSegment(m+1,n,false);
+    if (m < n)
+    {
+        if (flag == true) System.Console.Write("[ ");
+        System.Console.Write($"{m}, ");
+        NumbersOnSegment(m + 1, n, false);
+    }
+        if (m > n)
+    {
+        if (flag == true) System.Console.Write("[ ");
+        System.Console.Write($"{n}, ");
+        NumbersOnSegment(m, n + 1, false);
+    }
+
 }
 
-NumbersOnSegment(5,20);
+NumbersOnSegment(20, 5);
 System.Console.WriteLine("");
 
 //2
-    static int Ackermann(int n, int m)
-    {
-        if (n == 0) return m + 1;
-        if (m == 0) return Ackermann(n - 1, 1);
-        return Ackermann(n - 1, Ackermann(n, m - 1));
-    }
+static int Ackermann(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return Ackermann(n - 1, 1);
+    return Ackermann(n - 1, Ackermann(n, m - 1));
+}
 
-System.Console.WriteLine(Ackermann(2,5));
+System.Console.WriteLine(Ackermann(2, 5));
 
 System.Console.WriteLine("");
 
 //3
-int [] Array = new int[30];
+int[] Array = new int[30];
 
 Random rnd = new Random();
 System.Console.WriteLine("Исходный массив:");
-for(int i = 0; i < Array.Length; ++i)
+for (int i = 0; i < Array.Length; ++i)
 {
-    int rVal = rnd.Next(1,31);
+    int rVal = rnd.Next(1, 31);
     Array[i] = rVal;
     System.Console.Write($"{Array[i]} ");
 }
@@ -51,12 +61,12 @@ for(int i = 0; i < Array.Length; ++i)
 System.Console.WriteLine("\nRevArrayPrint:");
 void RevArrayPrint(int[] ArrToPrint, int i = 0)
 {
-    if(i == ArrToPrint.Length - 1)
+    if (i == ArrToPrint.Length - 1)
     {
         System.Console.Write($"{ArrToPrint[i]}");
         return;
     }
-    RevArrayPrint(ArrToPrint,i+1);
+    RevArrayPrint(ArrToPrint, i + 1);
     System.Console.Write($" {ArrToPrint[i]}");
 }
 
